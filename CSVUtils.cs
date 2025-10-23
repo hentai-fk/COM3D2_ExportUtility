a52dd639658937380f3ccc534aaaada280853120
lbwengineer <3305707402@qq.com>
2025-10-24 04:06:39 +0800

    提交项目

---
+++ CSVUtils.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM3D2_ExportUtility
+{
+    internal class CSVUtils
+    {
+        public static string[][] LoadFromCSVFile(string file_name)
+        {
+            return LoadFromCSVString(File.ReadAllText(file_name));
+        }
+
+        public static string[][] LoadFromCSVString(string file_string)
+        {
+            int file_length = file_string.Length;
+
+            // read char by char and when a , or \n, perform appropriate action
+            int cur_file_index = 0; // index in the file
+            var all_lines = new List<List<string>>();
+            List<string> cur_line = new List<string>(); // current line of data
+            int max_column = 0;
+            StringBuilder cur_item = new StringBuilder("");
+            bool inside_quotes = false; // managing quotes
+            while (cur_file_index < file_length)
+            {
+                char c = file_string[cur_file_index++];
+
+                switch (c)
+                {
+                    case '"':
+                        if (!inside_quotes)
+                        {
+                            inside_quotes = true;
+                        }
+                        else
+                        {
+                            if (cur_file_index == file_length)
+                            {
+                                // end of file
+                                inside_quotes = false;
+                                goto case '\n';
+                            }
+                            else if (file_string[cur_file_index] == '"')
+                            {
+                                // double quote, save one
+                                cur_item.Append("\"");
+                                cur_file_index++;
+                            }
+                            else
+                            {
+                                // leaving quotes section
+                                inside_quotes = false;
+                            }
+                        }
+                        break;
+                    case '\r':
+                        // ignore it completely
+                        break;
+                    case ',':
+                        goto case '\n';
+                    case '\n':
+                        if (inside_quotes)
+                        {
+                            // inside quotes, this characters must be included
+                            cur_item.Append(c);
+                        }
+                        else
+                        {
+                            // end of current item
+                            cur_line.Add(cur_item.ToString());
+                            cur_item.Length = 0;
+                            if (c == '\n' || cur_file_index == file_length)
+                            {
+                                // 读取到了行结尾
+                                max_column = Math.Max(max_column, cur_line.Count);
+                                all_lines.Add(cur_line);
+                                cur_line = new List<string>();
+                            }
+                        }
+                        break;
+                    default:
+                        // other cases, add char
+                        cur_item.Append(c);
+                        break;
+                }
+            }
+
+            var result = new string[all_lines.Count][];
+            for (int i = 0; i < all_lines.Count; i++)
+            {
+                result[i] = new string[max_column];
+                for (int j = 0; j < all_lines[i].Count; j++)
+                {
+                    result[i][j] = all_lines[i][j];
+                }
+                for (int j = all_lines[i].Count; j < max_column; j++)
+                {
+                    // 多余的空行用空字符填充
+                    result[i][j] = "";
+                }
+            }
+
+            return result;
+        }
+
+        public static string[][] LoadFromFileNei(AFileBase file)
+        {
+            using (var csv = new CsvParser())
+            {
+                if (!csv.Open(file))
+                    return new string[0][];
+                var result = new string[csv.max_cell_y][];
+                for (int y = 0; y < csv.max_cell_y; y++)
+                {
+                    result[y] = new string[csv.max_cell_x];
+                    for (int x = 0; x < csv.max_cell_x; x++)
+                    {
+                        result[y][x] = csv.GetCellAsString(x, y);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public static void SaveArray2CSVFile(string file_name, string[][] array)
+        {
+            using (var output = new StreamWriter(File.OpenWrite(file_name), Encoding.UTF8))
+            {
+                output.Write(TurnArray2CSVString(array));
+            }
+        }
+
+        public static string TurnArray2CSVString(string[][] array)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    var text = array[i][j];
+                    if (text.Contains("\"") || text.Contains("\n"))
+                        text = "\"" + text.Replace("\"", "\"\"") + "\"";
+                    sb.Append(text);
+                    if (j != array[i].Length - 1)
+                    {
+                        sb.Append(",");
+                    }
+                }
+                if (i != array.Length - 1)
+                {
+                    sb.AppendLine();
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace COM3D2_ExportUtility
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SetDllDirectory("Libs");
+
+            string baseDirectory = ".";
+            string outputDirectory = Path.Combine(baseDirectory, "OutputDirectory");
+
+            Console.WriteLine("搜索目录: " + baseDirectory);
+            Console.WriteLine("输出目录: " + outputDirectory);
+
+            foreach (var arc in Directory.GetFiles(baseDirectory, "*.arc", SearchOption.AllDirectories))
+            {
+                if (!arc.StartsWith(baseDirectory, StringComparison.OrdinalIgnoreCase))
+                    continue;
+                var outputRel = outputDirectory + arc.Substring(baseDirectory.Length, arc.Length - 4 - baseDirectory.Length);
+
+                Console.WriteLine("\n加载档案: " + arc);
+
+                var fs = new FileSystemArchive();
+                fs.AddArchive(arc);
+                var list = fs.GetFileListAtExtension("*");
+
+                Console.WriteLine($"发现 {list.Count()} 项结果");
+
+                foreach (var item in list)
+                {
+                    var outputAbs = Path.Combine(outputRel, item);
+                    var arcItemName = Encoding.Default.GetString(Encoding.UTF8.GetBytes(item));
+
+                    Directory.CreateDirectory(Directory.GetParent(outputAbs).FullName);
+
+                    if (outputAbs.EndsWith(".nei", StringComparison.OrdinalIgnoreCase))
+                    {
+                        outputAbs = outputAbs.Substring(0, outputAbs.Length - 4) + ".csv";
+                        OutputNei(fs, arcItemName, outputAbs);
+                    }
+                    else
+                    {
+                        using (var fd = fs.FileOpen(arcItemName))
+                        {
+                            if (fd.IsValid())
+                            {
+                                if (outputAbs.EndsWith(".ks", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    using (var output = new StreamWriter(File.OpenWrite(outputAbs), Encoding.UTF8))
+                                    {
+                                        output.Write(NUty.SjisToUnicode(fd.ReadAll()));
+                                    }
+                                }
+                                else
+                                {
+                                    using (var output = File.OpenWrite(outputAbs))
+                                    {
+                                        var buffer = fd.ReadAll();
+                                        output.Write(buffer, 0, buffer.Length);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("导出文件: " + outputAbs);
+                }
+
+                fs.Dispose();
+            }
+        }
+
+        static void OutputNei(FileSystemArchive fs, string filename, string outputAbs)
+        {
+            using (var fd = fs.FileOpen(filename))
+            {
+                CSVUtils.SaveArray2CSVFile(outputAbs, CSVUtils.LoadFromFileNei(fd));
+            }
+        }
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern bool SetDllDirectory(string lpPathName);
+    }
+}
